845076e591a93aeb6aebcfeb82771cf145be8127
adrian <adrianfj@gmail.com>
2019-03-11 16:39:18 -0400

    Created main app

---
+++ django-server/api_django/main/serializers.py
@@ -0,0 +1,11 @@
+
+#TODO: merge from reservations
+
+class EVOwnerSerializer(serializers.ModelSerializer):
+
+class EVCarSerializer(serializers.ModelSerializer):
+
+class CSOwnerSerializer(serializers.ModelSerializer):
+
+
+class CSSerializer(serializers.ModelSerializer):
