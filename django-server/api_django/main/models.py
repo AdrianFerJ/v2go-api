a7f549c75bcf94a2c81c4e070226b95cbb829b19
adrian <adrianfj@gmail.com>
2019-03-13 02:03:26 -0400

    Create CS list view and tests

--- django-server/api_django/main/models.py
+++ django-server/api_django/main/models.py
@@ -1,5 +1,12 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser, Group
+# from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import GEOSGeometry, fromstr
+
+from django.shortcuts import reverse
+from django.contrib.auth.models import AbstractUser
+
+import datetime
+import hashlib 
 
 
 # Create your models here.
@@ -7,11 +14,80 @@ from django.contrib.auth.models import AbstractUser, Group
 # Finder
 class User(AbstractUser): 
     pass
+    # groups = (RIDER , OWNER  , VOLT_MANAGER (?) )
 
-#     #User = models.OneToOneField(User, related_name="main_user", primary_key=True)
-#     # groups = models.OneToOneField(Group, related_name="main_groups", primary_key=True)
-#     #related_name argument to the definition for 'VoltUser.groups' or 'User.groups'
 
+class ChargingStation(models.Model):
+    """ CS model is shared by all different apps / components of this system"""
+    #TODO move const declaratios  (status, charge_level) to a dif file
+    AVAILABLE       = 'AVAILABLE'
+    RESERVED        = 'RESERVED'
+    UNAVAILABLE     = 'UNAVAILABLE'
+    OUTOFSERVICE    = 'OUT_OF_SERVICE'
+    LEVEL_1         = 'LEVEL_1'
+    LEVEL_2         = 'LEVEL_2'
+    FASTDC          = 'FAST_DC'
+
+    STATUSES = (
+        (AVAILABLE, AVAILABLE),
+        (RESERVED, RESERVED),
+        (UNAVAILABLE, UNAVAILABLE),
+        (OUTOFSERVICE, OUTOFSERVICE),
+    )
+
+    nk           = models.CharField(max_length=32, unique=True, db_index=True)
+    name         = models.CharField(max_length=255, blank=True)
+    external_id  = models.CharField(max_length=100, blank=True)
+    #TODO: this should be a foreign key to link with cs_owner model
+    # cs_host   = models.IntegerField()
+
+    CHARGE_LEVEL = (
+        (LEVEL_1, LEVEL_1),
+        (LEVEL_2, LEVEL_2),
+        (FASTDC, FASTDC),
+    )
+    charge_level = models.CharField(max_length=32, choices=CHARGE_LEVEL, default=LEVEL_2)
+    tarif_text   = models.CharField(max_length=100, blank=True)
+
+    address      = models.CharField(max_length=150) 
+    location     = models.CharField(max_length=150, default='') 
+    city         = models.CharField(max_length=50, blank=True)
+    province     = models.CharField(max_length=50, blank=True)
+    country      = models.CharField(max_length=50, blank=True)
+    postal_code  = models.CharField(max_length=10, blank=True)
+    #TODO: lat,lng should be mandatory (blank=False) before pushing to production
+    lat          = models.FloatField(null=True, blank=True) 
+    lng          = models.FloatField(null=True, blank=True) 
+    geo_location = models.PointField(null=True)
+
+    created      = models.DateTimeField(auto_now_add=True)
+    updated      = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nk
+
+    def get_absolute_url(self):
+        return reverse('cStation:cStation_detail', kwargs={'cStation_nk': self.nk})
+    
+    def create_geo_location(self):
+        self.geo_location = fromstr(f'POINT({self.lng} {self.lat})', srid=4326)
+
+    def save(self, **kwargs):
+        if not self.nk:
+            now = datetime.datetime.now()
+            secure_hash = hashlib.md5()
+            secure_hash.update(
+                #TODO  add :{self.owner} OR owner_nk OR owner foreign key
+                f'{now}:{self.address}'.encode(
+                    'utf-8'))
+            self.nk = secure_hash.hexdigest()
+        if not self.geo_location:
+            try:
+                self.create_geo_location()
+            except:
+                #TODO: add test (if lat-lng not valid, etc) and add Log if geolocation not created
+                pass
+        super().save(**kwargs)
 
 # Finder
 # class ChargingStation(models.Model):
@@ -23,3 +99,4 @@ class User(AbstractUser):
 #     #rename to EVehicle
 
 #     #TODO renamte owner to ev_owner
+# 
--- django-server/api_django/main/urls.py
+++ django-server/api_django/main/urls.py
@@ -1,3 +1,15 @@
+from django.urls import re_path, path
+
+from main import views #ChargingStationListView, ChargingStationDetailView, ChargingStationTopNearListView
+
+app_name = 'main'
+
+urlpatterns = [
+    # path('host/', ChargingStationListView.as_view({'get': 'list'}), 
+    #     name='host_cs_list'),
+    path('host/', views.ChargingStationListView.as_view(), name='host_cs_list'),
+]
+
 #TODO review this, from reservation
 # path('ev_owners', EVOwnerSerializerView.as_view({'get': 'list'}), name='ev_owners_list'),
 # path('ev_cars', EVCarSerializerView.as_view({'get': 'list'}), name='ev_cars_list'),
