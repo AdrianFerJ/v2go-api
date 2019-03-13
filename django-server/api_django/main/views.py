a2f9eba2b9e2401f9de2ab7dc4cdee0c9907ed97
adrian <adrianfj@gmail.com>
2019-03-13 14:46:29 -0400

    Refactored all components of app votl_finder

--- django-server/api_django/main/views.py
+++ django-server/api_django/main/views.py
@@ -1,23 +1,43 @@
-from django.shortcuts import render
-from .models import ChargingStation
-from rest_framework import generics, permissions, status, views, viewsets
+from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth.forms import AuthenticationForm 
 
 from main.models import ChargingStation
-from .serializers import ChargingStationSerializer # , UserSerializer, GeoCStationSerializer
+from main.serializers import ChargingStationSerializer, UserSerializer#, GeoCStationSerializer
 
-
+from rest_framework import generics, permissions, status, views, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-from rest_framework import mixins
 from rest_framework import generics
 
 """ 
     Views
 """
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class LogInView(views.APIView):
+    def post(self, request):
+        form = AuthenticationForm(data=request.data)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user=form.get_user())
+            return Response(UserSerializer(user).data)
+        else:
+            return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogOutView(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, *args, **kwargs):
+        logout(self.request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class ChargingStationList(generics.ListCreateAPIView):
-    #TODO enable create by changing inheritance from ListAPIView to ListCreateAPIView
     #TODO add permission_classes = (permissions.IsAuthenticated,) AND TEST
     #TODO host can only see her own CS, but no CSs owned by another host
     queryset = ChargingStation.objects.all()
@@ -25,6 +45,8 @@ class ChargingStationList(generics.ListCreateAPIView):
 
 
 class ChargingStationDetail(generics.RetrieveUpdateDestroyAPIView):
+    #TODO add permission_classes = (permissions.IsAuthenticated,) AND TEST
+    #TODO host can only see her own CS, but no CSs owned by another host
     lookup_field = 'nk'
     lookup_url_kwarg = 'cs_nk'
     queryset = ChargingStation.objects.all()
--- django-server/api_django/volt_finder/urls.py
+++ django-server/api_django/volt_finder/urls.py
@@ -1,17 +1,12 @@
 from django.urls import re_path, path
 
-from .apis import ChargingStationListView, ChargingStationDetailView, ChargingStationTopNearListView
+from .views import ChargingStationTopNearListView
 
 app_name = 'volt_finder'
 
 urlpatterns = [
-    re_path(r'^$', ChargingStationListView.as_view({'get': 'list'}), 
-        name='cStations_list'),
-    re_path(r'^(?P<cStation_nk>\w{32})/detail/$', ChargingStationDetailView.as_view(
-        {'get': 'retrieve'}), name='cStation_detail'), 
     path('near_poi/<poi_location>/', ChargingStationTopNearListView.as_view(
-        # {'get': 'list'}), name='cStations_near_poi'),
-        {'get': 'top_cs_near_poi'}), name='cStations_near_poi'),
+        {'get': 'top_cs_near_poi'}), name='cs_near_poi'),
 ]
 
 
