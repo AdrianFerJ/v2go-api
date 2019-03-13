1c3d72eb2d0f05884ee725d5213100ada78196cc
adrian <adrianfj@gmail.com>
2019-03-13 11:52:51 -0400

    minor changes

--- django-server/api_django/main/views.py
+++ django-server/api_django/main/views.py
@@ -13,19 +13,13 @@ from rest_framework import status
 from rest_framework import mixins
 from rest_framework import generics
 
+
 class ChargingStationList(generics.ListAPIView):
     #TODO enable create by changing inheritance from ListAPIView to ListCreateAPIView
+    #TODO add permission_classes = (permissions.IsAuthenticated,) AND TEST
+    #TODO host can only see her own CS, but no CSs owned by another host
     queryset = ChargingStation.objects.all()
     serializer_class = ChargingStationSerializer
-
-
-# class ChargingStationDetail(viewsets.ReadOnlyModelViewSet):  
-#     """ Get's a32char nk and returns CS's detail info that matches the nk """
-    # lookup_field = 'nk'
-    # lookup_url_kwarg = 'cs_nk'
-#     permission_classes = (permissions.IsAuthenticated,)
-#     queryset = ChargingStation.objects.all()
-#     serializer_class = ChargingStationSerializer
 
 
 class ChargingStationDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -33,20 +27,3 @@ class ChargingStationDetail(generics.RetrieveUpdateDestroyAPIView):
     lookup_url_kwarg = 'cs_nk'
     queryset = ChargingStation.objects.all()
     serializer_class = ChargingStationSerializer
-
-
-# class ChargingStationDetail(mixins.RetrieveModelMixin,
-#                     mixins.UpdateModelMixin,
-#                     mixins.DestroyModelMixin,
-#                     generics.GenericAPIView):
-#     queryset = ChargingStation.objects.all()
-#     serializer_class = ChargingStationSerializer
-
-#     def get(self, request, *args, **kwargs):
-#         return self.retrieve(request, *args, **kwargs)
-
-#     def put(self, request, *args, **kwargs):
-#         return self.update(request, *args, **kwargs)
-
-#     def delete(self, request, *args, **kwargs):
-#         return self.destroy(request, *args, **kwargs)
