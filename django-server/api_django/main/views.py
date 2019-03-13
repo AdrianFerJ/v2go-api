190cd0a46c74f92ffb8fabbb705b42f47535e3f3
adrian <adrianfj@gmail.com>
2019-03-13 02:27:13 -0400

    Fix get_absolute_url()

--- django-server/api_django/main/views.py
+++ django-server/api_django/main/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from .models import ChargingStation
+
 from rest_framework import generics, permissions, status, views#, viewsets
 
 from main.models import ChargingStation
 from .serializers import ChargingStationSerializer # , UserSerializer, GeoCStationSerializer
 
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
 class ChargingStationListView(generics.ListAPIView):
     #TODO enable create by changing inheritance from ListAPIView to ListCreateAPIView
