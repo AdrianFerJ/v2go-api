ad34af9e13921a25abdf8bc93981bdfeca1f53ad
Niroigen Suntharam <niroigen@gmail.com>
2019-03-16 18:27:30 -0400

    Able to view EV detail view

--- django-server/api_django/main/urls.py
+++ django-server/api_django/main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import re_path, path
 from rest_framework.urlpatterns import format_suffix_patterns
 from main.views import ChargingStationList, ChargingStationDetail, CSHostList, CSHostDetail, \
-					   EVOwnerList, EVList
+					   EVOwnerList, EVOwnerDetail, EVList, EVDetail
 
 app_name = 'main'
 
@@ -13,9 +13,10 @@ urlpatterns = [
     path('cs_hosts/<cs_host_nk>/', CSHostDetail.as_view(),
     	name='host_cs_detail'),
     path('ev_owners/', EVOwnerList.as_view(), name='ev_owner_list'),
-    path('ev_owners/<ev_owner_nk>', EVOwnerList.as_view(),
+    path('ev_owners/<ev_owner_nk>', EVOwnerDetail.as_view(),
     	name='ev_owner_detail'),
     path('ev/', EVList.as_view(), name='ev_list'),
+    path('ev/<ev_nk>', EVDetail.as_view(), name='ev_detail')
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
 
