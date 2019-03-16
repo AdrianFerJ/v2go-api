7ee359931885b4f8aab37a21fdcf8d66dfb83aec
Niroigen Suntharam <niroigen@gmail.com>
2019-03-16 19:46:49 -0400

    Adding EOF

--- django-server/api_django/main/urls.py
+++ django-server/api_django/main/urls.py
@@ -19,9 +19,3 @@ urlpatterns = [
     path('ev/<ev_nk>', EVDetail.as_view(), name='ev_detail')
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
-
-#TODO review this, from reservation
-# path('ev_owners', EVOwnerSerializerView.as_view({'get': 'list'}), name='ev_owners_list'),
-# path('ev_cars', EVCarSerializerView.as_view({'get': 'list'}), name='ev_cars_list'),
-# path('cs_owners', CSOwnerSerializerView.as_view({'get': 'list'}), name='cs_owners_list'),
-# path('css', CSSerializerView.as_view({'get': 'list'}), name='css_list'),
