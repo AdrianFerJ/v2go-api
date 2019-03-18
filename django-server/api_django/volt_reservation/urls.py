dce3cb8800f2fb03502d56d9d74ce7972463320e
Niroigen Suntharam <niroigen@gmail.com>
2019-03-18 10:26:02 -0400

    Removing the SERIALIZER in CSEventView and EVEventView

--- django-server/api_django/volt_reservation/urls.py
+++ django-server/api_django/volt_reservation/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import CSEventSerializerView, EVEventSerializerView
+from .views import CSEventView, EVEventView
 
 app_name = 'volt_reservation'
 
 urlpatterns = [
-	path('cs_events', CSEventSerializerView.as_view({'get': 'list'}), name='cs_events_list'),
-	path('cs_events/available/<datestr>', CSEventSerializerView.as_view({'get': 'get_available_charging_station'}), name='available'),
-	path('ev_events', EVEventSerializerView.as_view({'get': 'list'}), name='ev_events_list'),
+	path('cs_events', CSEventView.as_view({'get': 'list'}), name='cs_events_list'),
+	path('cs_events/available/<datestr>', CSEventView.as_view({'get': 'get_available_charging_station'}), name='available'),
+	path('ev_events', EVEventView.as_view({'get': 'list'}), name='ev_events_list'),
 ]
