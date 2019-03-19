cc609b964731b8b951dd33cba96dcdce457c655c
adrian <adrianfj@gmail.com>
2019-03-19 15:41:13 -0400

    Fixed merge conflict in main/admin.py

--- django-server/api_django/main/admin.py
+++ django-server/api_django/main/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import EV, Driver, ChargingStation, CSHost
 
+
+class UserAdmin(admin.ModelAdmin):
+    # display entries as table, with the following fields
+    list_display = ('username', 'is_staff')
+
+
+class ChargingStationAdmin(admin.ModelAdmin):
+    list_display = ('nk', 'address')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(ChargingStation, ChargingStationAdmin)
 admin.site.register(EV)
 admin.site.register(Driver)
-admin.site.register(ChargingStation)
 admin.site.register(CSHost)
