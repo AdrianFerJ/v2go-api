b406f706287357d803843bfcd6cdbe4455337742
adrian <adrianfj@gmail.com>
2019-03-22 12:17:38 -0400

    Improved cs.nk matching and isolated helper from view

--- django-server/api_django/volt_finder/views.py
+++ django-server/api_django/volt_finder/views.py
@@ -2,6 +2,8 @@ from rest_framework import permissions, status, views, viewsets
 from rest_framework.response import Response
 
 from volt_finder.serializers import GeoCStationSerializer
+from volt_finder import finder_helpers as fh
+
 from main.serializers import ChargingStationSerializer
 from main.models import ChargingStation
 from . import mygooglemaps as gg
@@ -20,22 +22,12 @@ class ChargingStationTopNearListView(viewsets.ReadOnlyModelViewSet):
         # Get All charging stations and pass their locations to getNearest       
 
         all_cs = self.queryset
-
         cs_addresses = [i.address for i in all_cs]
-        top_cs = gg.getNearestCS(poi_location, cs_addresses)
+        
+        gg_top_cs = gg.getNearestCS(poi_location, cs_addresses)        
+        fh.match_cs_nk_based_on_address(gg_top_cs, all_cs)
 
-        # Match CS info from queryset to top_cs, based on address
-        # TODO conver this for loop into a helper function and move it to that file
-        for cs in top_cs:
-            xnk = next((x for x in all_cs if x.address == cs.destination_addresses), None)
-            if xnk != None:
-                cs.nk = str(xnk)
-            else:
-                pass
-            #TODO: USE try and except
-            #TODO: add other relevant CS fields (update CS data class in gg module)
-           
-        serializer = GeoCStationSerializer(top_cs, many=True)
+        serializer = GeoCStationSerializer(gg_top_cs, many=True)
 
         return Response(serializer.data)
 
