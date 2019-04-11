a49318f71354f6bc51acd2ac696491ec70af6e09
Niroigen Suntharam <niroigen@gmail.com>
2019-04-11 01:02:42 -0400

    Using constants.AVAILABLE

--- django-server/api_django/volt_reservation/services.py
+++ django-server/api_django/volt_reservation/services.py
@@ -1,8 +1,9 @@
 from .models import EventCS
 import datetime
+from main import constants
 
 class ReservationService(object):
 	@staticmethod
 	def get_available_event_cs(start_datetime, end_datetime):
 		return EventCS.objects.filter(
-			status='AVAILABLE', startDateTime__range=(start_datetime, end_datetime))
+			status=constants.AVAILABLE, startDateTime__range=(start_datetime, end_datetime))
