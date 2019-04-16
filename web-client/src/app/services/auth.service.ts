a350b670e9c74176c0a2fce1a57a4e04ad00ad25
adrian <adrianfj@gmail.com>
2019-04-16 16:02:45 -0400

    Updated auth service

--- web-client/src/app/services/auth.service.ts
+++ web-client/src/app/services/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
 
+export class User {
+  // Data model to store user info (a JSON-serialized string returned by API) in the UI
+  constructor(
+    public id?: number,
+    public username?: string,
+    public first_name?: string,
+    public last_name?: string,
+    public group?: string,
+    public photo?: any
+  ) {}
+
+  // Convenience method to handle the conversion from JSON to data object 
+  static create(data: any): User { 
+    return new User(
+      data.id,
+      data.username,
+      data.first_name,
+      data.last_name,
+      data.group,
+      data.photo
+    );
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   constructor() { }
 }
