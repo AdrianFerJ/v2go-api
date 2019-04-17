fff76e9a4c86dbab20aed61c1f93a595076b9449
Niroigen Suntharam <niroigen@gmail.com>
2019-04-17 16:42:25 -0400

    Removing repeated imports

--- django-server/api_django/main/views.py
+++ django-server/api_django/main/views.py
@@ -7,9 +7,6 @@ from main.serializers import ChargingStationSerializer, UserSerializer, Electric
 from rest_framework import generics, permissions, status, views, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import viewsets
-from rest_framework import status
-from rest_framework import generics
 
 """ 
     Views
