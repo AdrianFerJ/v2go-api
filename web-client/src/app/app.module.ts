911d937433028ab679be4e55fefe412786ade740
adrian <adrianfj@gmail.com>
2019-04-22 18:20:56 -0400

    Create driver component

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule }          from '@angular/platform-browser';
 import { NgModule }               from '@angular/core';
 import { RouterModule, Routes }   from '@angular/router';
 import { FormsModule }            from '@angular/forms';  
-// import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';  
 import { HttpClientModule }       from '@angular/common/http';  
+// import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';  
 
 import { AuthService }            from './services/auth.service';  
 
@@ -12,11 +12,13 @@ import { AppComponent }           from './app.component';
 import { SignUpComponent }        from './components/sign-up/sign-up.component';
 import { LogInComponent }         from './components/log-in/log-in.component';
 import { LandingComponent }       from './components/landing/landing.component';
+import { DriverComponent }        from './components/driver/driver.component';
 
 
 const appRoutes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
   { path: 'log-in', component: LogInComponent },
+  { path: 'driver', component: DriverComponent },
   { path: '', component: LandingComponent }
   // SAMPLE
   // { path: 'hero/:id',      component: HeroDetailComponent },
@@ -43,7 +45,8 @@ const appRoutes: Routes = [
     AppComponent,
     SignUpComponent,
     LogInComponent,
-    LandingComponent
+    LandingComponent,
+    DriverComponent
   ],
   providers: [ 
     AuthService,
