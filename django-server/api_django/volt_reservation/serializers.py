7c24519f2c309bf81060a485ba454eb78fa38e09
Niroigen Suntharam <niroigen@gmail.com>
2019-05-05 16:16:55 -0400

    Displaying name serialized data instead of nk

--- django-server/api_django/volt_reservation/serializers.py
+++ django-server/api_django/volt_reservation/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import generics, serializers
 
 
 class EventCSSerializer(serializers.ModelSerializer):
-	cs = serializers.CharField(source='cs.nk', read_only=True)
+	cs = serializers.CharField(source='cs.name', read_only=True)
 	created 		= serializers.DateTimeField(format="%Y-%m-%d %H:%M")
 	updated 		= serializers.DateTimeField(format="%Y-%m-%d %H:%M")
 	startDateTime	= serializers.DateTimeField(format="%Y-%m-%d %H:%M")
@@ -17,7 +17,7 @@ class EventCSSerializer(serializers.ModelSerializer):
 class EventEVSerializer(serializers.ModelSerializer):
 	# event_cs = serializers.CharField(source='event_cs.nk', read_only=True)
 	event_cs = EventCSSerializer(many=False, read_only=True)
-	ev = serializers.CharField(source='ev.nk', read_only=True)
+	ev = serializers.CharField(source='ev.model', read_only=True)
 
 	class Meta:
 		model = EventEV
