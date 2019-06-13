c6baa665dbab5ba114ab5809a05e7c7cd0b586b7
Niroigen Suntharam <niroigen@gmail.com>
2019-06-13 17:15:40 -0400

    Removing factory method calls

--- django-server/api_django/volt_reservation/services.py
+++ django-server/api_django/volt_reservation/services.py
@@ -5,6 +5,20 @@ from main import constants
 
 class ReservationService(object):
 	@staticmethod
+	def get_event_cs_for_cs(cs_nk, start_date_time_range):
+		return EventCS.objects.filter(
+			cs__nk=cs_nk,
+			startDateTime__range=start_date_time_range
+		)
+
+	@staticmethod
+	def get_available_event_cs(start_date_time_range):
+		return EventCS.objects.filter(
+			startDateTime__range=start_date_time_range,
+			status=constants.AVAILABLE
+		)
+
+	@staticmethod
 	def get_completed_event_ev(ev):
 		return EventEV.objects.filter(
 			status=constants.COMPLETED,
