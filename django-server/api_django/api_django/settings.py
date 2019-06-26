98610de1c25c18a5764a00a387ffb99365ec5f34
adrian <adrianfj@gmail.com>
2019-06-25 23:51:04 -0400

    Set up nginx for production

--- django-server/api_django/api_django/settings.py
+++ django-server/api_django/api_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('v2go_SECRETE_KEY', 'SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver', 'localHost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Custom user model
 # AUTH_USER_MODEL = 'volt_finder.User'
