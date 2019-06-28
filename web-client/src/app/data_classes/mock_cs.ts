fa738de320d80064a2995e94c8e251c349daf772
Niroigen Suntharam <niroigen@gmail.com>
2019-06-28 17:44:26 -0400

    Removing all angular files since it has been moved to another repo

--- web-client/src/app/data_classes/mock_cs.ts
+++
@@ -1,9 +0,0 @@
-import { ChargingStation }       from './chargingStation';
-import { CStationFactory }       from '../testing/factories';
-
-export const STATIONS: ChargingStation[] = [
-  CStationFactory.create(), 
-  CStationFactory.create()
-//   { id: 11, nk: ???, name: 'Mr. Nice' },
-//   { id: 12, nk: ???, name: 'Narco' },
-];
